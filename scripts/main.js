6ff5ac88028c2ebb0124e85034064a6810a840ae
Lazir Pascual <lazirpascual@gmail.com>
2021-05-30 03:55:10 -0400

    create the update read-status button

--- scripts/main.js
+++ scripts/main.js
@@ -16,26 +16,31 @@ function addBookToLibrary(title, author, pages, isRead) {
 }
 
 // display all current books
-function addBookToPage() {
+function addBooksToPage() {
     myLibrary.forEach(function (book) {
         const singleBook = document.createElement('div');
         singleBook.setAttribute('style', 'white-space: pre;');
         singleBook.textContent =  book.title + "\r\n";
         singleBook.textContent += "By: " + book.author + "\r\n";
         singleBook.textContent += "Number of pages: " +book.pages + "\r\n";
-        singleBook.classList.add('books');
-        bookContainer.appendChild(singleBook);
+        singleBook.classList.add((book.isRead == 'Read' ? 'books' : 'books-unread'));
 
-        /* add delete button to book */
+        /* add delete button */
         const deleteBtn = document.createElement('button');
         deleteBtn.textContent = "Delete";
         deleteBtn.classList.add('delete-button');
 
-        /* create data attribute that saves index of current book so we can delete it later */
-        deleteBtn.setAttribute('data-index', myLibrary.indexOf(book));
-        deleteBtn.setAttribute('id', myLibrary.indexOf(book));
+        /* add a status button */
+        const statusBtn = document.createElement('button');
+        statusBtn.textContent = book.isRead;
+        statusBtn.classList.add('status-button');
 
-        singleBook.appendChild(deleteBtn);
+        /* create data attributes that saves index of current book so we can delete and update it later */
+        deleteBtn.setAttribute('data-index', myLibrary.indexOf(book));
+        statusBtn.setAttribute('data-status', myLibrary.indexOf(book));
+
+        singleBook.appendChild(statusBtn); // add status button to book
+        singleBook.appendChild(deleteBtn); // add delete button to book
         bookContainer.appendChild(singleBook);
     });
 }
@@ -47,9 +52,14 @@ function removeAllBooks(parent) {
     }
 }
 
-//remove a book object in the array
+// remove a book object in the array
 function removeBookInArray(index) {
     myLibrary.splice(index, 1);
+}
+
+// toggle read status
+function toggleStatus(index) {
+    myLibrary[index].isRead = (myLibrary[index].isRead == "Read" ? 'Unread' : 'Read');
 }
 
 // add book to library on button click
@@ -58,9 +68,10 @@ addBookBtn.addEventListener('click', function () {
     let title = document.getElementById("title").value;
     let author = document.getElementById("author").value;
     let pages = document.getElementById("pages").value;
-    addBookToLibrary(title, author, pages, "read");
-    removeAllBooks(bookContainer);  // remove all books in the node
-    addBookToPage();                // re-display all books
+    let status = document.getElementById("status-input").value;
+    addBookToLibrary(title, author, pages, status);
+    removeAllBooks(bookContainer);   // remove all books in the node
+    addBooksToPage();                // re-display all books
 });
 
 // delete book on button click
@@ -70,7 +81,17 @@ bookContainer.addEventListener('click', function(e) {
     if (e.target.classList == 'delete-button') {  
         removeBookInArray(bookToDelete);          // remove the book we want to delete in the array
         removeAllBooks(bookContainer);            // remove all books in the node
-        addBookToPage();                          // re-display all books
+        addBooksToPage();                         // re-display all books
+    }
+});
+
+// update read status on button click
+bookContainer.addEventListener('click', function(e) {
+    let bookToUpdate = e.target.getAttribute('data-status');
+    if (e.target.classList == 'status-button') {  
+        toggleStatus(bookToUpdate);
+        removeAllBooks(bookContainer);            // remove all books in the node
+        addBooksToPage();                         // re-display all books
     }
 });
 
